45028638f9fa4b64e5e87a1c6994b6d75248cb74
ItsNathan21 <nathanpo@andrew.cmu.edu>
2025-09-02 00:33:43 -0400

    test

---
+++ kernel/boot/boot.s
@@ -0,0 +1,36 @@
+.set ALIGN,     1 << 0
+.set MEMINFO,   1 << 1
+.set FLAGS,     ALIGN | MEMINFO 
+.set MAGIC,     0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC 
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+_stack_bottom:
+.skip 16384
+_stack_top:
+
+.section .text
+.global _start 
+.type _start, @function 
+_start:
+    cmp $0x2badb002, %eax
+    jne _hang
+    push %ebx
+    push %eax
+    call kmain
+
+_hang:    
+    mov %eax, %edi
+    call shitballs
+    jmp .
+
+.size _start, . - _start
+
+
